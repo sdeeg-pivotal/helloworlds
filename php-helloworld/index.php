d9fc72cf4abf92c87d814c9280b7990cc71d9f52
Scott  Deeg <sdeeg@pivotal.io>
2016-06-30 10:17:51 -0700

    adding php

---
+++ php-helloworld/index.php
@@ -0,0 +1,22 @@
+<?php
+	header('Content-type: text/html; charset=utf-8');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Hello PHP</title>
+</head>
+<body>
+
+<div>
+<p>
+<?php
+echo "Hello PHP";
+?>
+</p>
+</div>
+
+</body>
+
+
